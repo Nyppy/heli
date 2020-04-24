74f8c82230f4b2a491daa5cf19081fa6a0f87bd5
Grigorii <fastmotion1111@gmail.com>
2020-04-24 15:58:02 +0300

    refactor code

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,13 +4,66 @@ import {withRouter, Link} from "react-router-dom";
 
 import "./Footer.css"
 import Modal from './modal/modal'
+
+import PolicyComponent  from "./ComponetFooter/policy"
+import ConditionComponent from "./ComponetFooter/conditions"
+import OfferComponent from "./ComponetFooter/offer"
 import logo from "../../assets/img/logo-header.png"
 import jscorplogos from "../../assets/img/jscorplogos.png"
 import { FaVk,FaFacebookF,FaInstagram,FaTelegramPlane } from "react-icons/fa";
 
 
+
 class Main extends Component {
+
+
+    state={
+        policy:false,
+        conditions:false,
+        offer:false
+    }
+
+
+
+    onCloseModal = () => (
+        this.setState({
+            policy:false,
+            conditions:false,
+            offer:false
+        })
+    )
+
+
+
+
     render() {
+
+
+
+        const { policy, conditions, offer}= this.state
+
+
+        let modalContent
+        if(policy) {
+            modalContent = <PolicyComponent/>
+        }
+        else if(conditions) {
+            modalContent = <ConditionComponent/>
+        }
+        else if  (offer) {
+            modalContent = <OfferComponent/>
+        }
+
+        if(policy || conditions || offer) {
+            return (
+                <Modal
+                    modalContent = {modalContent}
+                    onCloseModal ={this.onCloseModal}
+                />
+            )
+        }
+
+
         return (
             <footer>
                 <div className={"container-footer"}>
@@ -32,11 +85,28 @@ class Main extends Component {
                         </div>
 
                         <div className="footer-elem-2">
-                                <Modal  text={"Политика конфиденциальности"}/>
-                            <span>|</span>
-                                <Modal  text={"Условия использования"}/>
-                            <span>|</span>
-                                <Modal  text={"Условия оферты"}/>
+                                {/*<Modal  text={"Политика конфиденциальности"}/>*/}
+                            {/*<span>|</span>*/}
+                                {/*<Modal  text={"Условия использования"}/>*/}
+                            {/*<span>|</span>*/}
+                                {/*<Modal  text={"Условия оферты"}/>*/}
+                            <button className={"footer-elem-2__item"} onClick={()=>this.setState({ policy: true })} >
+                                <div className={"footer-elem-2__items"}>
+                                    Политика конфиденциальности
+                                </div>
+                            </button>
+                                <span>|</span>
+                            <button className={"footer-elem-2__item"} onClick={()=>this.setState({ conditions: true })} >
+                                <div className={"footer-elem-2__items"}>
+                                    Условия использования
+                                </div>
+                            </button>
+                            {/*<span>|</span>*/}
+                            {/*<button className={"footer-elem-2__item"} onClick={()=>this.setState({ offer: true })} >*/}
+                                {/*<div className={"footer-elem-2__items"}>*/}
+                                    {/*Условия оферты*/}
+                                {/*</div>*/}
+                            {/*</button>*/}
                         </div>
                         <div className="footer-elem-3">
                              <div className="footer-elem-3-item1">© {new Date().getFullYear()} HELI technology ltd.</div>
@@ -55,6 +125,8 @@ class Main extends Component {
                     </div>
                 </div>
             </footer>
+
+
         )
     }
 }
